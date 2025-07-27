f469783bdf1dd0f68d188c2912eec270d2281422
TJ Keat <tjkeat123@gmail.com>
2025-07-28 01:07:32 +0800

    feat: add navigation island

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Contact from './components/Contact'
 import SideBullet from './components/SideBullet'
+import NavigationIsland from './components/NavigationIsland'
 
 function App() {
   return (
-    <div className="h-screen overflow-y-scroll snap-y snap-mandatory">
+    <div className="h-screen overflow-y-scroll snap-y snap-mandatory scroll-smooth">
+      <NavigationIsland />
       <SideBullet />
       <Hero />
       <About />
---
+++ src/components/NavigationIsland.tsx
@@ -0,0 +1,26 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHouse, faUser, faEnvelope } from "@fortawesome/free-regular-svg-icons"
+
+function NavigationIsland() {
+
+  const navElement = ({ icon, text, href }: { icon: any, text: string, href: string }) => {
+    return (
+      <a href={href} className="flex flex-row items-center group overflow-hidden transition-all duration-500 ease-in-out p-3 md:p-4">
+        <FontAwesomeIcon icon={icon} className="text-black text-xl flex-shrink-0" />
+        <span className="hidden md:inline text-black text-sm font-body ml-2 max-w-0 opacity-0 group-hover:max-w-xs group-hover:opacity-100 transition-all duration-500 ease-in-out whitespace-nowrap overflow-hidden">{text}</span>
+      </a>
+    )
+  }
+
+  return (
+    <div className="fixed flex flex-row bottom-10 md:bottom-5 left-1/2 transform -translate-x-1/2 z-10 bg-white rounded-full opacity-50 hover:opacity-100 transition-opacity duration-300 shadow-lg">
+      {navElement({ icon: faHouse, text: "Home", href: "#hero" })}
+      <div className="w-px h-6 bg-gray-300 self-center"></div>
+      {navElement({ icon: faUser, text: "About", href: "#about" })}
+      <div className="w-px h-6 bg-gray-300 self-center"></div>
+      {navElement({ icon: faEnvelope, text: "Contact Me", href: "#contact-me" })}
+    </div>
+  )
+}
+
+export default NavigationIsland
